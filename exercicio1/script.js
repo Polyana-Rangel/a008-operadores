4fd2ddd2baf951f28782a76718ad1234e9d23ec6
Polyana-Rangel <polyanarangel17@gmail.com>
2022-08-30 14:25:07 -0300

    exercicio2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,9 @@
-let par = Number (prompt ("insira um número pra: "));
+let num1 = Number (prompt ("insira um número: "));
 console.log  (par);
 
 let div = par / 2;
 let resto = par % 2;
-
+// testei c varios numeros pares e nao teve resto.
 console.log(div,resto);
 
+// qnd testei c numero impar apareceu o resto.
